136834869e22eda7a40346e49f50e55acbeaf7e0
Ian Davis <idavis@innovatian.com>
2010-03-04 08:41:13 -0500

    Updating build files for all targets.

--- build/build-release.cmd
+++ build/build-release.cmd
@@ -1,5 +1,14 @@
-..\contrib\nant\nant.exe -buildfile:Ninject.build clean %1 %2 %3 %4 %5 %6 %7 %8
-..\contrib\nant\nant.exe -buildfile:Ninject.build package-source %1 %2 %3 %4 %5 %6 %7 %8
-..\contrib\nant\nant.exe -buildfile:Ninject.build "-D:build.config=release" "-D:build.platform=net-3.5" package-bin %1 %2 %3 %4 %5 %6 %7 %8
-..\contrib\nant\nant.exe -buildfile:Ninject.build "-D:build.config=release" "-D:build.platform=silverlight-3.0" package-bin %1 %2 %3 %4 %5 %6 %7 %8
-..\contrib\nant\nant.exe -buildfile:Ninject.build "-D:build.config=release" "-D:build.platform=mono-2.0" package-bin %1 %2 %3 %4 %5 %6 %7 %8
+..\contrib\nant\nant.exe -help
+..\contrib\nant\nant.exe -v -buildfile:Ninject.build clean %1 %2 %3 %4 %5 %6 %7 %8
+..\contrib\nant\nant.exe -v -buildfile:Ninject.build clean %1 %2 %3 %4 %5 %6 %7 %8
+..\contrib\nant\nant.exe -v -buildfile:Ninject.build package-source %1 %2 %3 %4 %5 %6 %7 %8
+..\contrib\nant\nant.exe -v -buildfile:Ninject.build clean %1 %2 %3 %4 %5 %6 %7 %8
+..\contrib\nant\nant.exe -v -buildfile:Ninject.build "-D:build.config=release" "-D:build.platform=net-3.5" package-bin %1 %2 %3 %4 %5 %6 %7 %8
+..\contrib\nant\nant.exe -v -buildfile:Ninject.build clean %1 %2 %3 %4 %5 %6 %7 %8
+..\contrib\nant\nant.exe -v -buildfile:Ninject.build "-D:build.config=release" "-D:build.platform=silverlight-3.0" "-D:build.proxy=cdp2" package-bin %1 %2 %3 %4 %5 %6 %7 %8
+..\contrib\nant\nant.exe -v -buildfile:Ninject.build clean %1 %2 %3 %4 %5 %6 %7 %8
+..\contrib\nant\nant.exe -v -buildfile:Ninject.build "-D:build.config=release" "-D:build.platform=mono-2.0" "-D:build.proxy=linfu" package-bin %1 %2 %3 %4 %5 %6 %7 %8
+..\contrib\nant\nant.exe -v -buildfile:Ninject.build clean %1 %2 %3 %4 %5 %6 %7 %8
+..\contrib\nant\nant.exe -v -buildfile:Ninject.build "-D:build.config=release" "-D:build.platform=net-3.5" "-D:build.proxy=linfu" package-bin %1 %2 %3 %4 %5 %6 %7 %8
+..\contrib\nant\nant.exe -v -buildfile:Ninject.build clean %1 %2 %3 %4 %5 %6 %7 %8
+..\contrib\nant\nant.exe -v -buildfile:Ninject.build "-D:build.config=release" "-D:build.platform=net-3.5" "-D:build.proxy=cdp2" package-bin %1 %2 %3 %4 %5 %6 %7 %8
