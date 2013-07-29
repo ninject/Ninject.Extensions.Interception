7e0fb3f6dc6b0ccea54c8d9455acf49a78f2a3d9
Remo Gloor <remo.gloor@bbv.ch>
2013-07-29 13:44:59 +0200

    Removed SelfBoundTypesDeclaringInterceptorsOnGenericMethodsAreIntercepted from LinFu because this is already tested in the base tests

--- src/Ninject.Extensions.Interception.Test/LinFuBaseTests.cs
+++ src/Ninject.Extensions.Interception.Test/LinFuBaseTests.cs
@@ -1,32 +1,8 @@
 #if !SILVERLIGHT
 namespace Ninject.Extensions.Interception
 {
-    using FluentAssertions;
-    using LinFu.DynamicProxy;
-    using Ninject.Extensions.Interception.Fakes;
-    using Ninject.Extensions.Interception.Infrastructure.Language;
-    using Ninject.Extensions.Interception.Interceptors;
-    using Xunit;
-    
     public class LinFuBaseTests : LinFuInterceptionContext
     {
-        [Fact]
-        public void SelfBoundTypesDeclaringInterceptorsOnGenericMethodsAreIntercepted()
-        {
-            using (var kernel = CreateDefaultInterceptionKernel())
-            {
-                kernel.Bind<ObjectWithGenericMethod>().ToSelf();
-                var obj = kernel.Get<ObjectWithGenericMethod>();
-                obj.Should().NotBeNull();
-                typeof(IProxy).IsAssignableFrom(obj.GetType()).Should().BeTrue();
-
-                FlagInterceptor.Reset();
-                string result = obj.ConvertGeneric("", 42);
-
-                result.Should().Be("42");
-                FlagInterceptor.WasCalled.Should().BeTrue();
-            }
-        }
     }
 }
 #endif
