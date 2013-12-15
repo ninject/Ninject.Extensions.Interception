a8218a4d65555564675e3617dfff898aaa9c4a70
Scott Xu <scott-xu@msn.com>
2013-12-15 15:29:10 +0800

    Unit Test for Explicit Interface Implementation

---
+++ src/Ninject.Extensions.Interception.Test/DynamicProxy2ImplicitInterfaceTest.cs
@@ -0,0 +1,23 @@
+﻿namespace Ninject.Extensions.Interception
+{
+    using FluentAssertions;
+    using Ninject.Extensions.Interception.Fakes;
+    using Xunit;
+
+    public class DynamicProxy2ImplicitInterfaceTest : DynamicProxy2BaseTests
+    {
+        [Fact]
+        public void InterceptedObjectShouldKeepImplementingOrigialInterfaces()
+        {
+            using (StandardKernel kernel = CreateDefaultInterceptionKernel())
+            {
+                kernel.Bind<IBase>().To<ImplicitDerived>();
+
+                var obj = kernel.Get<IBase>();
+
+                obj.Should().NotBeNull();
+                obj.Should().BeAssignableTo<IDerived>();
+            }
+        }
+    }
+}
