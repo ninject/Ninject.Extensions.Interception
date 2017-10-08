98ef360111c323b28f4a68f1914d4f7db6598a2a
Scott Xu <scott-xu@msn.com>
2017-10-08 15:39:21 +0800

    Disable ParallelizeTestCollections

--- src/Ninject.Extensions.Interception.Test/AsyncInterceptionContext.cs
+++ src/Ninject.Extensions.Interception.Test/AsyncInterceptionContext.cs
@@ -1,7 +1,5 @@
-#if !NET_35 && !SILVERLIGHT
 namespace Ninject.Extensions.Interception
 {
-    using System.Threading;
     using System.Threading.Tasks;
     using FluentAssertions;
     using Ninject.Extensions.Interception.Infrastructure.Language;
@@ -10,24 +8,7 @@ namespace Ninject.Extensions.Interception
     public abstract class AsyncInterceptionContext : InterceptionTestContext
     {
         [Fact]
-        public void AsyncMethodsCanBeIntercepted()
-        {
-            using (var kernel = this.CreateDefaultInterceptionKernel())
-            {
-                BeforeAfterCallOrderInterceptor.Reset();
-
-                kernel.Bind<AsyncService>().ToSelf().Intercept().With<BeforeAfterCallOrderInterceptor>();
-
-                var service = kernel.Get<AsyncService>();
-                var task = service.DoAsync();
-                task.Wait();
-
-                BeforeAfterCallOrderInterceptor.Order.Should().Be("Before_Action_AfterCompleted_");
-            } 
-        }
-
-        [Fact]
-        public void AsyncMethodsWithReturnValueCanBeIntercepted()
+        public async Task AsyncMethodsCanBeIntercepted()
         {
             using (var kernel = this.CreateDefaultInterceptionKernel())
             {
@@ -36,16 +17,31 @@ namespace Ninject.Extensions.Interception
                 kernel.Bind<AsyncService>().ToSelf().Intercept().With<BeforeAfterCallOrderInterceptor>();
                 var service = kernel.Get<AsyncService>();
 
-                var task = service.DoAsyncInt();
-                task.Wait();
- 
+                await service.DoAsync();
+
                 BeforeAfterCallOrderInterceptor.Order.Should().Be("Before_Action_AfterCompleted_");
-                task.Result.Should().Be(42);
             }
         }
 
         [Fact]
-        public void AsyncMethodsWithReturnValue_InterceptorsCanChangeTheResult()
+        public async Task AsyncMethodsWithReturnValueCanBeIntercepted()
+        {
+            using (var kernel = this.CreateDefaultInterceptionKernel())
+            {
+                BeforeAfterCallOrderInterceptor.Reset();
+
+                kernel.Bind<AsyncService>().ToSelf().Intercept().With<BeforeAfterCallOrderInterceptor>();
+                var service = kernel.Get<AsyncService>();
+
+                var result = await service.DoAsyncInt();
+
+                BeforeAfterCallOrderInterceptor.Order.Should().Be("Before_Action_AfterCompleted_");
+                result.Should().Be(42);
+            }
+        }
+
+        [Fact]
+        public async Task AsyncMethodsWithReturnValue_InterceptorsCanChangeTheResult()
         {
             using (var kernel = this.CreateDefaultInterceptionKernel())
             {
@@ -54,39 +50,13 @@ namespace Ninject.Extensions.Interception
                 kernel.Bind<AsyncService>().ToSelf().Intercept().With<IncreaseResultInterceptor>();
                 var service = kernel.Get<AsyncService>();
 
-                var task = service.DoAsyncInt();
-                task.Wait();
+                var result = await service.DoAsyncInt();
 
                 BeforeAfterCallOrderInterceptor.Order.Should().Be("Before_Action_AfterCompleted_");
-                task.Result.Should().Be(43);
+                result.Should().Be(43);
             }
         }
 
-#if NET_40
-        public class AsyncService
-        {
-            public virtual Task<int> DoAsyncInt()
-            {
-                BeforeAfterCallOrderInterceptor.ActionCalled();
-                var result = new Task<int>(
-                    () =>
-                        {
-                            Thread.Sleep(100);
-                            return 42;
-                        });
-                result.Start();
-                return result;
-            }
-
-            public virtual Task DoAsync()
-            {
-                BeforeAfterCallOrderInterceptor.ActionCalled();
-                var result = new Task(() => Thread.Sleep(100));
-                result.Start();
-                return result;
-            }
-        }
-#else
         public class AsyncService
         {
             public virtual async Task<int> DoAsyncInt()
@@ -112,7 +82,6 @@ namespace Ninject.Extensions.Interception
                 return Task.Delay(100).ContinueWith((t, o) => 42, null);
             }
         }
-#endif
 
         public class BeforeAfterCallOrderInterceptor : AsyncInterceptor
         {
@@ -158,4 +127,3 @@ namespace Ninject.Extensions.Interception
         }
     }
 }
-#endif
